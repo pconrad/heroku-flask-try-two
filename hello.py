266f0fa0db7332785d28117e564adbc3e636da0e
Phill Conrad <pconrad@cs.ucsb.edu>
2015-08-18 08:27:05 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+def ftoc(ftempString):
+    return (ftemp - 32 ) * (5.0/9.0)
+    
+@app.route('/ftoc/<ftempString>')
+def convertFtoC(ftempString)
+    ftemp = 0.0
+    try:
+        ftemp = float(ftempString)
+        ctemp = ftoc(temp)
+        return "In Farenheit: " + ftempString + " In Celsius " + str(ctemp) 
+    except ValueError:
+        return "Sorry.  Could not convert " + ftempString + " to a number"
+
+    
