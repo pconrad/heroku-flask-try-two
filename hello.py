ece102d1e531206230719476d4421c83d9dafffc
Phill Conrad <pconrad@cs.ucsb.edu>
2015-08-20 10:07:15 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -20,6 +20,13 @@ def convertFtoC(ftempString):
     except ValueError:
         return "Sorry.  Could not convert " + ftempString + " to a number"
 
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 @app.route('/ftocForm')
 def ftocForm():
     return "stub"
