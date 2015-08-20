debce61a09ecdfd101c7981d2add21ff13c1fa4f
Phill Conrad <pconrad@cs.ucsb.edu>
2015-08-20 10:51:35 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-url_for('static', filename='style.css')
 
 @app.route('/')
 def helloRoot():
@@ -31,3 +30,4 @@ def hello(name=None):
 @app.route('/ftocForm')
 def ftocForm():
     return "stub"
+
